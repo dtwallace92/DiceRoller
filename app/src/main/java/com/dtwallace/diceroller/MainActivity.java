c84e913b3ab1037594c4e453c0823b1085a31daf
Dustin Wallace <dtwallace92@gmail.com>
2018-10-19 18:05:28 -0400

    added user choice of dice size and other small features

--- app/src/main/java/com/dtwallace/diceroller/MainActivity.java
+++ app/src/main/java/com/dtwallace/diceroller/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    int diceSize;
     int result;
 
     @Override
@@ -17,15 +20,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final EditText diceSizeEditText = findViewById(R.id.diceSizeEditText);
         Button rollButton = findViewById(R.id.rollButton);
         final TextView resultText = findViewById(R.id.resultText);
 
+        /**
+         * When Roll button is clicked take chosen dice number and roll for a random number within
+         * that range.
+         */
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Random r = new Random();
-                result = r.nextInt(19) +1;
-                //resultText.setText(result.toString());
+                String editValue = diceSizeEditText.getText().toString();
+                diceSize = Integer.parseInt(editValue);
+
+                // Make sure user input a proper dice size
+                if (diceSize > 0) {
+                    result = r.nextInt(diceSize) + 1;
+                    resultText.setText(String.valueOf(result));
+                } else {
+                    Toast.makeText(MainActivity.this, "Dice must be larger than zero!", Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
